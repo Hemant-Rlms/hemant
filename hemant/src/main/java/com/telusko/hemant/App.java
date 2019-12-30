7d019704c8fea84eebc57a43c4fa736fffe9cf3a
Hemant <Hemant@LAPTOP-0C7EJ2MP>
2019-12-30 23:23:07 +0530

    This is my First Commit

---
+++ hemant/src/main/java/com/telusko/hemant/App.java
@@ -0,0 +1,13 @@
+package com.telusko.hemant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I love You" );
+    }
+}
